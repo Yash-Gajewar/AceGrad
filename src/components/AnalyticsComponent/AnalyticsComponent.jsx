6c4987df259aa41cc63c7a4baa655335b9f1fb67
Yash-Gajewar <yashgajewar06@gmail.com>
2024-04-08 02:33:47 +0530

    Analytics page updated

--- src/components/AnalyticsComponent/AnalyticsComponent.jsx
+++ src/components/AnalyticsComponent/AnalyticsComponent.jsx
@@ -1,7 +1,10 @@
-import React from 'react'
+import React, { useState } from 'react'
 import insights_icon from '../../assets/insights_icon.png'
 
 const AnalyticsComponent = (props) => {
+
+    const [analyticsSelected, setAnalyticsSelected] = useState(true)
+
     return (
 
 
@@ -14,7 +17,7 @@ const AnalyticsComponent = (props) => {
 
                 <div className='flex text-md font-sm p-2 w-full h-fit bg-indigo-50'>
                     <div>
-                        <img src={insights_icon} alt='insights icon' className='w-7 h-7 mr-2 cursor-pointer ml-2' 
+                        <img src={insights_icon} alt='insights icon' className='w-7 h-7 mr-2 cursor-pointer ml-2'
                             onClick={() => props.setCollapsed(!props.collapsed)}
                         />
 
@@ -24,23 +27,32 @@ const AnalyticsComponent = (props) => {
                         Your Speaking Insights
                     </div>
                 </div>
-                
+
                 {/* tab parent holder */}
                 <div className='flex bg-white shadow-lg mt-5 w-full justify-center items-end cursor-pointer'>
 
                     {/* tab holder */}
-                    <div className='flex bg-indigo-50 rounded-2xl w-2/3 m-5 justify-around items-center p-3'>
+                    <div className='flex bg-indigo-50 rounded-2xl w-2/3 m-5 justify-around items-center'>
 
                         {/* tab 1 */}
-                        <div className='flex bg-white w-1/2 h-full justify-center items-center' >
+                        <div
+                            className={`flex w-1/2 justify-center items-center p-2 rounded-2xl ${analyticsSelected ? 'bg-white shadow-inner' : ''}`}
+                            onClick={() => setAnalyticsSelected(true)}
+                        >
                             Analytics
                         </div>
 
 
                         {/* tab 2 */}
-                        <div className='flex justify-center items-center w-1/2'>
-                            Suggestions
+
+                        <div
+                            className={`flex w-1/2 justify-center items-center p-2 rounded-2xl ${!analyticsSelected ? 'bg-white shadow-inner' : ''}`}
+                            onClick={() => setAnalyticsSelected(false)}
+                        >
+                            Suggesstions
                         </div>
+
+
                     </div>
                 </div>
 
